50c2cc88da9643860bf54c686c0f9ddbdefe1164
Corbin Fonville <corbin.fonville@gmail.com>
2025-05-03 16:49:18 -0500

    api hookup

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,9 +5,6 @@ const router = Router()
 import headersMiddleware from './middleware/headers.js'
 router.use(headersMiddleware)
 
-router.use('/ping', async (req, res) => {
-  res.status(200).send('pong')
-})
 router.use('/v1', require('/v1').default)
 
 export default router
--- src/handlers/api.ts
+++ src/handlers/api.ts
@@ -7,6 +7,8 @@ const api = serverless(app)
 app.get('/ping', (req, res) => {
   res.status(200).send({ message: 'pong' })
 })
+app.use('/api', require('../api/v1').default)
+
 const handler: serverless.Handler = async (event, context) => {
   return api(event, context)
 }

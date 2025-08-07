c7780ae259e42b314a13ecaf2ec8eab09b7fb0a2
Corbin Fonville <corbin.fonville@gmail.com>
2025-08-07 12:39:34 -0500

    init /api/v1/ping route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
+import v1 from './v1'
 
 const router = Router()
 
 import headersMiddleware from './middleware/headers.js'
 router.use(headersMiddleware)
 
-router.use('/v1', require('/v1').default)
+router.use('/v1', v1)
 
 export default router
--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,4 +2,8 @@ import { Router } from 'express'
 
 const router = Router()
 
+router.get('/ping', (req, res) => {
+  res.status(200).send({ message: 'pong' })
+})
+
 export default router
